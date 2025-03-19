beccdbc3e3182d9af03d84a3b7d3d190744cb836
Adriel Renato <adriel.renato26@gmail.com>
2025-03-19 19:44:44 -0300

    Criando o design pattern de strategy para chamar as validações da regra de negócio de forma separada

--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -34,30 +34,7 @@ public class AdocaoService {
         var pet = petRepository.getReferenceById(dto.idPet());
         var tutor = tutorRepository.getReferenceById(dto.idTutor());
 
-        if (pet.getAdotado()) {
-            throw new ValidacaoException("Pet já foi adotado!");
-        } else {
-            List<Adocao> adocoes = repository.findAll();
-            for (Adocao a : adocoes) {
-                if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                    throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-                }
-            }
-            for (Adocao a : adocoes) {
-                if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                    throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-                }
-            }
-            for (Adocao a : adocoes) {
-                int contador = 0;
-                if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.APROVADO) {
-                    contador = contador + 1;
-                }
-                if (contador == 5) {
-                    throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
-                }
-            }
-        }
+        // Irei chamar as validações nesse exato momento
 
         var adocao = new Adocao();
         adocao.setData(LocalDateTime.now());
